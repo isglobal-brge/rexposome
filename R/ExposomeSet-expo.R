149469011a6a3eafee5b6c181054d15c1984a2d9
Carles Hernandez <carles.hernandez@isglobal.org>
2017-03-02 17:09:38 +0100

    Try to solve a bug at 'expos' accessor.

--- R/ExposomeSet-expo.R
+++ R/ExposomeSet-expo.R
@@ -2,7 +2,7 @@ setMethod(
     f = "expos",
     signature = "ExposomeSet",
     definition = function(object) {
-        expos <- t(assayDataElement(object, "exp"))
+        expos <- data.frame(t(assayDataElement(object, "exp")))
         type <- fData(object)[colnames(expos), "_type"]
         for(ii in 1:length(type)) {
             message(ii, ": ", type[ii])
