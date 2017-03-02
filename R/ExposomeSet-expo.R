df06ad1d75a631d11fd3736d936ccb431a77d1e6
Carles Hernandez <carles.hernandez@isglobal.org>
2017-03-02 16:51:57 +0100

    Try to solve a bug at 'expos' accessor.

--- R/ExposomeSet-expo.R
+++ R/ExposomeSet-expo.R
@@ -6,7 +6,7 @@ setMethod(
         type <- fData(exps)[colnames(expos), "_type"]
         for(ii in 1:length(type)) {
             expos[expos[ , ii] == "NA" , ii] <- NA
-            if(type == "numeric") {
+            if(type[ii] == "numeric") {
                 expos[ , ii] <- as.numeric(expos[ , ii])
             } else {
                 expos[ , ii] <- as.factor(expos[ , ii])
