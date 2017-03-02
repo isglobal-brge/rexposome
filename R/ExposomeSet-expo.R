1c04bee0de6fcf5019fe394fc42e932977c477bd
Carles Hernandez <carles.hernandez@isglobal.org>
2017-03-02 17:06:02 +0100

    Try to solve a bug at 'expos' accessor.

--- R/ExposomeSet-expo.R
+++ R/ExposomeSet-expo.R
@@ -3,8 +3,9 @@ setMethod(
     signature = "ExposomeSet",
     definition = function(object) {
         expos <- t(assayDataElement(object, "exp"))
-        type <- fData(exps)[colnames(expos), "_type"]
+        type <- fData(object)[colnames(expos), "_type"]
         for(ii in 1:length(type)) {
+            message(ii, ": ", type[ii])
             expos[expos[ , ii] == "NA" , ii] <- NA
             if(type[ii] == "numeric") {
                 expos[ , ii] <- as.numeric(expos[ , ii])
