ded6a6caf255c0a3d578c4272924cf641878de63
carleshf <carles.hernandez@isglobal.org>
2017-03-06 10:23:25 +0100

    Removed class 'ResultSet', moved to 'omicRexposome'.

--- R/00_AllClasses.R
+++ R/00_AllClasses.R
@@ -200,35 +200,3 @@ setClass(
     samples = "character"
   )
 )
-
-#' Class ResultSet
-#'
-#' Class \code{ResultSet} encapsulates the results from the association and
-#' integration processes developed in \code{rexposome}. This include the
-#' functions \link{assoc_snp}, \link{assoc_genexp}, \link{assoc_methy},
-#' \link{assoc_prot} and  \link{crossomics}.
-#'
-#' @name ResultSet
-#' @aliases ResultSet-class
-#' @rdname ResultSet-class
-#' @exportClass ResultSet
-#' @slot fun_origin Character containing the function that creates the object.
-#' @slot class_origin Character containing the class of the objects used to
-#' create the object.
-#' @slot names Character with the names of the datasets in the
-#' \link{MultiDataSet} used to create the object
-#' @slot results List containing the results of the association/integration.
-#' @slot fData List containing the feature-data of the original objects.
-#' @slot sva 0 if no correction process was applied. 1 if SVA was applied.
-#' process(es).
-setClass(
-  Class = "ResultSet",
-  representation = representation(
-    fun_origin = "character",
-    class_origin = "character",
-    names = "character",
-    results = "list",
-    fData = "list",
-    sva = "numeric"
-  )
-)
