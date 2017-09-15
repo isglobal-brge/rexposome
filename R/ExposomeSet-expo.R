9b665c6659bebfc79d6f5b644ecc561cbd8e18ca
Carles Hernandez-Ferrer <carles.hernandez@isglobal.org>
2017-09-15 17:21:41 +0200

    Added 'stop'.

--- R/ExposomeSet-expo.R
+++ R/ExposomeSet-expo.R
@@ -4,6 +4,9 @@ setMethod(
     signature = "ExposomeSet",
     definition = function(object) {
         ms <- data.frame(t(assayDataElement(object, "exp")))
+        if(!"_type" %in% colnames(ms)) {
+            stop("Invalid 'ExposomeSet'. It has no '_type' in 'fData'.")
+        }
         type <- fData(object)[colnames(ms), "_type"]
         for(ii in 1:length(type)) {
             if(type[ii] == "numeric") {
