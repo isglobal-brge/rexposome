a324f50d735d2af687253ed702daf260452f24ff
Carles Hernandez-Ferrer <carles.hernandez@isglobal.org>
2017-09-15 18:03:00 +0200

    Updated 'expos'.

--- R/ExposomeSet-expos.R
+++ R/ExposomeSet-expos.R
@@ -3,12 +3,17 @@ setMethod(
     f = "expos",
     signature = "ExposomeSet",
     definition = function(object) {
-        ms <- data.frame(t(assayDataElement(object, "exp")))
-        type <- fData(object)[colnames(ms), ".type"]
-        for(ii in 1:length(type)) {
+        ms <- data.frame(t(data.frame(object@assayData$exp)))
+        if( !".type" %in% colnames(fData(object))) {
+            stop("Invalid 'ExposomeSet'. It has no '.type' column in its 'fData'.")
+        }
+        type <- fData(object)[colnames(ms)[-(1:2)], ".type"]
+        expo.n <- rownames(fData(object))[-(1:2)]
+        names(type) <- expo.n
+        for(ii in expo.n) {
             if(type[ii] == "numeric") {
-                ms[ , ii] <- as.numeric(ms[ , ii])
-            } else {
+                ms[ , ii] <- as.numeric( as.character( ms[ , ii]) )
+            } else if(type[ii] == "factor") {
                 ms[ , ii] <- as.factor(ms[ , ii])
             }
         }
