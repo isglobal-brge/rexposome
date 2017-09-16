5c7b004cdd423a3f684d0383077a69559098f731
Carles Hernandez-Ferrer <carles.hernandez@isglobal.org>
2017-09-16 12:20:11 +0200

    ..

--- R/ExposomeSet-expos.R
+++ R/ExposomeSet-expos.R
@@ -7,8 +7,8 @@ setMethod(
         if( !".type" %in% colnames(fData(object))) {
             stop("Invalid 'ExposomeSet'. It has no '.type' column in its 'fData'.")
         }
-        type <- fData(object)[colnames(ms)[-(1:2)], ".type"]
-        expo.n <- rownames(fData(object))[-(1:2)]
+        type <- fData(object)[colnames(ms), ".type"]
+        expo.n <- rownames(fData(object))
         names(type) <- expo.n
         for(ii in expo.n) {
             if(type[ii] == "numeric") {
